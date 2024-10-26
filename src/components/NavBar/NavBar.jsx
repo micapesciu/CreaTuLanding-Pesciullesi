3276271407967eeae204f054417dc7be0a09cb94
Micaela Pesciullesi <micapesciu@gmail.com>
2024-10-26 05:05:33 -0300

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,21 +4,20 @@ import CartWidget from "../CartWidget/CartWidget.jsx"
 import { Link, NavLink } from "react-router-dom"
 
 const NavBar = () => {
-
     return (
-    <nav className="navbar">   
-        <Link to='/' className="logo">
-            <img src={logo} alt="Logo"/>
-        </Link>     
-        <div className="Categories">
-            <NavLink to={`/category/cuadros`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>CUADROS</NavLink>
-            <NavLink to={`/category/floreros`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>FLOREROS</NavLink>
-            <NavLink to={`/category/velas`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>VELAS</NavLink>
-        </div>
-        <CartWidget/>
-    </nav>
-    )
-
+        <nav className="navbar">   
+            <div className="logo">
+                <img src={logo} alt="Logo"/>  
+            </div>
+            <div className="Categories">
+                <NavLink to='/' className='Option'>INICIO</NavLink>
+                <NavLink to={`/category/cuadros`} className='Option'>CUADROS</NavLink>
+                <NavLink to={`/category/floreros`} className='Option'>FLOREROS</NavLink>
+                <NavLink to={`/category/velas`} className='Option'>VELAS</NavLink>
+            </div>
+            <CartWidget/>
+        </nav>
+    );
 }
 
 export default NavBar
