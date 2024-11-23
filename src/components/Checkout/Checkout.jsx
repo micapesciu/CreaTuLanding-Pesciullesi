1736197f09b2bed283f999949c68915fc7acea1a
Micaela Pesciullesi <micapesciu@gmail.com>
2024-11-22 23:07:00 -0300

    Arreglado la info de checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { useState,useContext  } from 'react'
 import { CartContext } from '../../context/CartContext'
-import { Timestamp, collection, addDoc } from 'firebase/firestore'
+import { Timestamp, collection, addDoc, getDocs } from 'firebase/firestore'
 import db from '../../db/db.js'
 import { Link } from 'react-router-dom'
 import './Checkout.css'
+import React, { useState, useContext, useEffect } from 'react';
 
 const Checkout = () => {
     const [dataForm, setDataForm] = useState({
@@ -14,8 +13,25 @@ const Checkout = () => {
     })
 
     const [idOrder, setIdOrder] = useState(null)
+    const [orderData, setOrderData] = useState(null);
 
     const {cart, totalPrice, clearCart} = useContext(CartContext)
+
+    useEffect(() => {
+        if (idOrder) {
+            const fetchOrderData = async () => {
+                const orderRef = collection(db, "orders")
+                const ordersSnapshot = await getDocs(orderRef)
+                ordersSnapshot.forEach((doc) => {
+                    if (doc.id === idOrder) {
+                        setOrderData(doc.data())
+                    }
+                })
+            }
+
+            fetchOrderData()
+        }
+    }, [idOrder])
 
     const handleChangeInput = (event) => { setDataForm({...dataForm, [event.target.name]: event.target.value})}
 
@@ -44,25 +60,28 @@ const Checkout = () => {
     }
 
     return (
-        <div class="CheckoutContainer">
-            {
-                idOrder === null ? (
+        <div className="CheckoutContainer">
+            {idOrder === null ? (
+                <>
                     <form onSubmit={handleSubmitForm}>
-                        <input type="text" name="fullname" value={dataForm.fullname} onChange={handleChangeInput} placeholder='Nombre' required />
-                        <input type="number" name="phone" value={dataForm.phone} onChange={handleChangeInput} placeholder='Celular' required />
-                        <input type="email" name="email" value={dataForm.email} onChange={handleChangeInput} placeholder='Email' required />
-                        <button type='submit'>Terminar mi compra</button>
+                        <input type="text" name="fullname" value={dataForm.fullname} onChange={handleChangeInput} placeholder="Nombre" required/>
+                        <input type="number" name="phone" value={dataForm.phone} onChange={handleChangeInput} placeholder="Celular" required/>
+                        <input type="email" name="email" value={dataForm.email} onChange={handleChangeInput} placeholder="Email" required/>
+                        <button type="submit">Terminar mi compra</button>
                         <Link to="/cart" className="OptionVaciar">Cancelar</Link>
                     </form>
-                ) : (
+                </>
+            ) : (
                 <div>
-                    <h1 >Orden generada correctamente</h1>
-                    <h2>¡Gracias por elegir Dalia! ​💖​</h2>
-                    <p>Enviamos a tu mail toda la info de la compra</p>
-                    <Link to="/" class="Option">Volver a la Tienda</Link>
+                    <h1>Orden generada correctamente</h1>
+                    <h2>¡Gracias por elegir Dalia! 💖</h2>
+                    <p>ID de Compra: {idOrder}</p>
+                    <p>Comprador: {dataForm.fullname}</p>
+                    <p>Email: {dataForm.email}</p>
+                    <p>Teléfono: {dataForm.phone}</p>
+                    <Link to="/" className="Option">Volver a la Tienda</Link>
                 </div>
-                )
-            }
+            )}
         </div>
     )
 }
